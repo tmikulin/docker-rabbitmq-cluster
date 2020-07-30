2d9670379016d83eb768d68eef0a46b2172be579
tmikulin <tmikulin@gmail.com>
2020-07-30 20:23:15 +0200

    add the first working draft for the create_cluster script

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,25 +1,39 @@
 #!/usr/bin/env bash
 
+set -e
+
+# define all the slave nodes here
+# just make sure you always have an quorum
+# 3,5,7 of nodes including the master
+slaves=(rabbit2 rabbit3)
+
+# check until rabbit1/master is up
+while ! nc -z localhost 5672;
+do
+  echo "Waiting for rabbit1 to wake up....";
+  sleep 1;
+done;
+
+echo "Rabbit1 node is up and ready for the incoming command..."
+
 # setup the HA mode on rabbit1 (master) node
-docker exec -it rabbit1 rabbitmqctl
+docker exec -it rabbit1 rabbitmqctl set_policy ha-all "^ha\." '{"ha-mode":"all", "ha-sync-mode":"automatic"}'
 
-# these specific command are for the "slave nodes/containers" only
-docker exec -it rabbit2 rabbitmqctl stop_app
+echo "The HA policy was successfully executed on the master rabbit1 node!"
 
-docker exec -it rabbit2 rabbitmqctl reset
+while ! (nc -z localhost 5673 && nc -z localhost 5674);
+do
+  echo "Waiting for for the rabbit slave to wake up...."
+  sleep 1
+done
 
-docker exec -it rabbit2 rabbitmqctl join_cluster rabbit@rabbit1
-
-docker exec -it rabbit2 rabbitmqctl start_app
-
-
-
-# if I will use a side container for the cluster management part
- - it will need rabbitmqctl command on different containers...
- - have to wait and know when every node is up and ready for taking commands....
-
-# on the master, wait until is up and add the ha mode command on it
-# in the slave, wait until is up and join them to the cluster....
-# check if the slaves need to join to the cluster after...
-
-# or just run the script from the host machine :)
+# these specific commands are for the "slave nodes/containers" ONLY
+for slave in ${slaves[*]}; do
+  docker exec -it "$slave" rabbitmqctl stop_app
+  sleep 1
+  docker exec -it "$slave" rabbitmqctl reset
+  sleep 1
+  docker exec -it "$slave" rabbitmqctl join_cluster rabbit@rabbit1
+  sleep 2
+  docker exec -it "$slave" rabbitmqctl start_app
+done
